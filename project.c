c516e6f67da05d16b6ba4bb9db07eae55a159711
Giwang Eka Risti <76390020+wangteam15@users.noreply.github.com>
2020-12-24 21:36:08 +0700

    Update project.c

--- project.c
+++ project.c
@@ -5,9 +5,40 @@
 struct data{
     int tgs,uts,uas;
     int tgsb,utsb,uasb;
-    float k;
+    float k, nilai;
     char ket[100];
-}db[1000];
+}db[1000], x;
+
+//Fungsi/function Proses shorting atau mengurutkan data yang akan diambil dari data yang terkecil
+/* PENJELASAN SORTING
+baris 26 : 	mendeklarasikan prosedur sort dengan tipe data int yang menerima masukan nilai jumlah banyaknya angka yang bertipe integer.
+baris 27 : 	mendeklarasikan variable i, dan j yang bertipe integer.
+baris 28 : 	melakukan perulangan i sebanyak n – 1.
+baris 30 : 	melakukan perulangan j dimana j adalah i - 1 dan sampai kurang dari n.
+baris 32 : 	kondisi dimana array pada index dengan nilai variable j lebih besar 
+			dari array pada index dengan nilai varible j+1 maka nilai j dimasukkan 
+			dengan nilai j+1.
+baris 34 : 	memasukkan nilai x dengan nilai db pada index ke j.
+baris 35 : 	mengganti nilai db pada index ke j dengan nilai db pada index ke j+1.
+baris 36 : 	mengganti nilai db pada index ke j+1 dengan nilai x.
+*/
+
+void Sort(int n){
+  int i, j;
+  for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(db[j].nilai > db[j+1].nilai)
+            {
+                x=db[j];
+                db[j]=db[j+1];
+                db[j+1]=x;
+            }
+        }
+    }
+}
+
 
 int main(){
     int n,i, j, l;
@@ -20,7 +51,7 @@ int main(){
         printf("Nilai Tugas : ");scanf("%d",&db[i].tgs);
         printf("Nilai UTS   : ");scanf("%d",&db[i].uts);
         printf("Nilai UAS   : ");scanf("%d",&db[i].uas);
-        printf("Nilai Huruf : ");scanf("%s",&db[i].ket);    
+        printf("Nilai Huruf : ");scanf("%s",&db[i].ket);
     }
     
     printf("\n");
@@ -30,31 +61,29 @@ int main(){
     printf("Masukan Nilai UTS Baru     : ");scanf("%d", &db[i].utsb);
     printf("Masukan Nilai UAS Baru     : ");scanf("%d", &db[i].uasb);
     
-    float nilai[100];
-    
     //menghitung nilai akhir
     //fungsi sqrt berguna untuk menghitung akar kuadrat
     //fungsi pow(x,y) berfungsi untuk mengakarkan angka x dengan angka y
     
     for(i=0;i<n;i++){
-    	nilai[i] = sqrt((pow((db[i].tgsb - db[i].tgs),2))+(pow((db[i].utsb - db[i].uts),2))+(pow((db[i].uasb - db[i].uas),2)));
-    	
+    	db[i].nilai = sqrt((pow((db[i].tgsb - db[i].tgs),2))+(pow((db[i].utsb - db[i].uts),2))+(pow((db[i].uasb - db[i].uas),2)));
     } 
- 	
+    
 	printf("===========================================================\n");
     printf("              Prediksi Kelas Data               \n");
     printf("============================================================\n");
     printf("NO | Nilai Tugas | Nilai UTS | Nilai UAS |   Kelas   | nilai |\n");
     printf("============================================================\n");
     for(i=0;i<n;i++){
-        printf("%d  | %d          | %d        | %d        | %s\t|%.2f  \n",i+1, db[i].tgs,db[i].uts, db[i].uas, db[i].ket, nilai[i]);
+        printf("%d  | %d          | %d        | %d        | %s\t|%.2f  \n",i+1, db[i].tgs,db[i].uts, db[i].uas, db[i].ket, db[i].nilai);
     }
     printf("============================================================\n\n");
     
     //memasukkan jumlah data yang akan diambil.
     printf("masukkan k: ");scanf("%d", &db[i].k);
     
-    //menampilkan hasil akhir program 
+    //menampilkan hasil akhir program
+    
     printf("\n");
     printf("=================================================\n");
     printf("            Hasil Prediksi Kelas Data               \n");
@@ -62,22 +91,9 @@ int main(){
     printf(" Nilai Tugas | Nilai UTS | Nilai UAS |   Kelas   \n");
     printf("=================================================\n");
     printf(" %d          | %d        | %d        ", db[i].tgsb,db[i].utsb, db[i].uasb);       
-    float d[n][0], jarak;
- 
-  //Proses shorting atau mengurutkan data yang akan diambil dari data yang terkecil
-  for (i = 0; i < (n-1); i++) {
-    for (j = 0; j < (n-1); j++) {
-      if (d[j][1] > d[j + 1][1]) {
-        jarak = d[j][1];
-        d[j][1] = d[j + 1][1];
-        d[j + 1][1] = jarak;
- 
-        l = d[j][0];
-        d[j][0] = d[j + 1][0];
-        d[j + 1][0] = l;
-      }
-    }
-  }
+
+  //Pemanggilan fungsi shorting
+   Sort(n);
   
   int baik = 0; 
   int cukup = 0; 
@@ -86,20 +102,19 @@ int main(){
 	//membandingkan kata apa yang sering muncul pada sebagian data yang telah diambil oleh variable K
 	//fungsi strcmp = digunakan untuk membandingkan string dengan string yang lainnya.
   for (i = 0; i < db[i].k; i++) {
-    l = d[i][0];
-    if (strcmp(db[l].ket, "baik") == 0) {
+    if (strcmp(db[i].ket, "baik") == 0) {
       baik++;
     }
-    if (strcmp(db[l].ket, "cukup") == 0) {
+    if (strcmp(db[i].ket, "cukup") == 0) {
       cukup++;
     }
-    if (strcmp(db[l].ket, "kurang") == 0) {
+    if (strcmp(db[i].ket, "kurang") == 0) {
       kurang++;
     }
   }
   
-  	//menampilkan hasil akhir kelasnya
-  printf("| %s \n", db[l].ket);
+  	//menampilkan hasil kelasnya
+      printf("| %s \n", db[i].ket);
 	
   if (baik > cukup && baik > kurang) {
     printf("\n baik");
